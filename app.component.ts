d73d1a794c4dad3a542b6bbd0e6807dd03319f18
ironwk85 <ironwk85@gmail.com>
2016-01-29 13:42:34 -0600

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -11,17 +11,33 @@ import {Food} from './food'
 	        <li *ngFor="#item of food">
 	          {{ item.name }}
 	        </li>
+	        <li>{{test}}</li>
 	      </ul>
+	      <div><input [(ngModel)]="test"></div>
+	      <button type="button" (click)="testF(test)">Click Me!</button>
+`	     
+/*
+	    <ul>
+	        <li *ngFor="#item of food">
+	          <div><input [(ngModel)]="item.name"></div>
+	        </li>
+	    </ul>
     `
+*/
 })
 export class AppComponent { 
 	title = 'World';
 	food = [
-  	new Food(1, 'Water'),
-  	new Food(13, 'Soda'),
-  	new Food(15, 'Tuna'),
-  	new Food(20, 'Bread')
-];
+	  	new Food(1, 'Water'),
+	  	new Food(13, 'Soda'),
+	  	new Food(15, 'Tuna'),
+	  	new Food(20, 'Bread')
+	];
+
+	testF(valor) {
+		this.food.push(new Food(100,valor));
+		this.test="";
+	}
 }
 /*
 export class AppComponent {
