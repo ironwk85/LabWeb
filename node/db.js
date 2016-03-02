50e76dd605bb667143fcd46d03a334a0cdbe3ed4
ironwk85 <ironwk85@gmail.com>
2016-03-02 17:07:13 -0600

    db.js

---
+++ node/db.js
@@ -0,0 +1,54 @@
+var mysql = require('mysql')
+  , async = require('async')
+
+var PRODUCTION_DB = 'app_prod_database'
+  , TEST_DB = 'app_test_database'
+
+exports.MODE_TEST = 'mode_test'
+exports.MODE_PRODUCTION = 'mode_production'
+
+var state = {
+  pool: null,
+  mode: null,
+}
+
+exports.connect = function(mode, done) {
+  state.pool = mysql.createPool({
+    host: 'mydbinstance.cquxjhmjewwr.us-west-2.rds.amazonaws.com',
+    user: 'master1',
+    password: 'M2H5hZmb',
+    port: '3306', 
+    database: 'mydb'
+  })
+
+  state.mode = 'mydb'
+  done()
+}
+
+exports.get = function() {
+  return state.pool
+}
+
+exports.fixtures = function(data,done) {
+  var pool = state.pool
+  if (!pool) return done(new Error('Missing database connection.'))
+
+  var names = Object.keys(data.tables)
+  async.each(names, function(name, cb) {
+    async.each(data.tables[name], function(row, cb) {
+      var keys = Object.keys(row)
+        , values = keys.map(function(key) { return "'" + row[key] + "'" })
+        console.log('INSERT INTO ' + name + ' (' + keys.join(',') + ') VALUES (' + values.join(',') + ')');
+      pool.query('INSERT INTO ' + name + ' (' + keys.join(',') + ') VALUES (' + values.join(',') + ')', cb)
+    }, cb)
+  }, done)
+}
+
+exports.drop = function(tables, done) {
+  var pool = state.pool
+  if (!pool) return done(new Error('Missing database connection.'))
+
+  async.each(tables, function(name, cb) {
+    pool.query('DELETE * FROM ' + name, cb)
+  }, done)
+}
