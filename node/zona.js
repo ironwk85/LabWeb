447f3fc9b6c84076fe60d016bbcde4a106c433fc
ironwk85 <ironwk85@gmail.com>
2016-03-02 17:09:24 -0600

    Create zona.js

---
+++ node/zona.js
@@ -0,0 +1,13 @@
+// Constructor
+function Zona(id,name,desc) {
+  // always initialize all instance properties
+  this.id = id;
+  this.name = name;
+  this.desc = desc;
+}
+// class methods
+//Zona.prototype.fooBar = function() {
+
+//};
+// export the class
+module.exports = Zona;
