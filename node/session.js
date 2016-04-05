33973619984cf41c2f189cb7e274275694cfeca8
ironwk85 <ironwk85@gmail.com>
2016-04-05 09:59:07 -0500

    Create session.js

---
+++ node/session.js
@@ -0,0 +1,18 @@
+exports.options = {
+  host: 'myHost',
+  user: 'myUser',
+  password: 'myPassword',
+  port: '3306', 
+  database: 'myDB',
+  cookie: {
+    expires: false
+  },
+  schema: {
+    tableName: 'sessions',
+    columnNames: {
+    session_id: 'session_id',
+    expires: 'expires',
+    data: 'data'
+    }
+  }
+};
